933f0299e8bd07202660c2ab89f8dd8adfacf099
cybertramp <paran_son@outlook.com>
2019-11-13 19:54:19 +0900

    gugudan added

--- basic/1_gugudan.c
+++ basic/1_gugudan.c
@@ -1,5 +1,36 @@
 #include<stdio.h>
 
+void gugudan(){
+	int x=0,y=0;
+	for(y=1;y<10;y++){
+		for(x=1;x<10;x++)
+			printf("%d x %d = %d\n",y,x,x*y);
+	
+		printf("============\n");
+	}
+}
+
+void gugudanWhile(){
+	int x=1,y=1;
+	while(1){
+		if(y>9){
+			break;
+		}
+
+		printf("%d x %d = %d\n",y,x,x*y);
+		x++;
+		if(x>9){
+			printf("============\n");
+			x=1;
+			y++;
+		}
+	}
+}
+
+
 void main(){
-	printf("");
+
+	gugudan();
+	gugudanWhile();
+
 }
